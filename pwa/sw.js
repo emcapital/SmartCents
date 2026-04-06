0c87b13ac2127fb8673ce33c711389e14a7b9f83
Ethan <ethanmontera@gmail.com>
2026-04-07 00:09:17 +0800

    first commit

---
+++ pwa/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'smartcents-cache-v1';
+const assets = [
+  '/',
+  '../index.html',
+  '../pages/auth/login.html',
+  '/manifest.json',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
